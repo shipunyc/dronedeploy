e71cfe1209546b835ab2f70d85134a1ba0d45094
shipunyc <mr.pu.shi@gmail.com>
2015-07-12 18:15:06 -0700

    Improves the algorithm: remove drones/handlers with no heartbeat directly, without depending on the socket connections.

--- manager.py
+++ manager.py
@@ -40,18 +40,61 @@ class Manager(object):
     self._drones_to_handlers = {}  # drone ids are keys
     self._handlers_to_drones = {}  # handler ids are keys
     self._available_handlers = []
+    self._drones_to_drop = []
 
   def start(self):
     """This is the main loop.
     """
+    print 'manager started (%d available handlers)' % len(self._available_handlers)
     while True:
       try:
         all_sockets = dict(self._poller.poll(PULLER_TIMEOUT_SECS * 1000))
       except KeyboardInterrupt:
         break
+      self._remove_non_existing_handlers_and_drones()
       self._process_handlers_socket(all_sockets)
-      self._remove_non_existing_handlers()
       self._process_drones_socket(all_sockets)
+
+  def _remove_non_existing_handlers_and_drones(self):
+    """Remove handlers or drones who are actually gone.
+    """
+    # Remove non-existing handlers.
+    for handler_id in (self._available_handlers + list(self._handlers_to_drones)):
+      idle_secs = time.time() - self._handlers_heartbeat[handler_id]
+      if idle_secs > MAX_IDLE_SECS:
+        self._handlers_heartbeat.pop(handler_id, None)
+        if handler_id in self._handlers_to_drones:
+          # Let the drone on the handler drop.
+          drone_id = self._handlers_to_drones[handler_id]
+          self._drones_heartbeat.pop(drone_id, None)
+          self._drones_to_handlers.pop(drone_id, None)
+          self._handlers_to_drones.pop(handler_id, None)
+          # Keep it in the list, so that we can notify the drone later.
+          self._drones_to_drop.append(drone_id)
+          print ('no heartbeat from drone_hander id=%s received for %0.1f ' +
+                 'seconds, dropping and dropping drone id=%s (%d ' +
+                 'available handler)') % (
+                 handler_id, idle_secs, drone_id, len(self._available_handlers))
+        else:
+          self._available_handlers.remove(handler_id)
+          print ('no heartbeat from drone_hander id=%s received for %0.1f ' +
+                 'seconds, (%d available handlers)') %(
+                handler_id, idle_secs, len(self._available_handlers))
+    # Remove non-existing drones.
+    for drone_id in list(self._drones_to_handlers):
+      idle_secs = time.time() - self._drones_heartbeat[drone_id]
+      if idle_secs > MAX_IDLE_SECS:
+        self._drones_heartbeat.pop(handler_id, None)
+        # Free the handler.
+        if drone_id in self._drones_to_handlers:
+          handler_id = self._drones_to_handlers[drone_id]
+          self._available_handlers.append(handler_id)
+          self._handlers_to_drones.pop(handler_id, None)
+          self._drones_to_handlers.pop(drone_id, None)
+          print ('no heartbeat from drone id=%s received for %0.1f seconds, '+
+                 'dropping and freeing drone_handler id=%s (%d ' +
+                 'available handlers)') % (
+                 drone_id, idle_secs, handler_id, len(self._available_handlers))
 
   def _process_handlers_socket(self, all_sockets):
     """Process connetions to self._handlers_socket
@@ -66,45 +109,24 @@ class Manager(object):
               handler_id, len(self._available_handlers))
         self._handlers_socket.send(b'FREE')
       elif handler_id in self._handlers_to_drones:
-        # Check for the heartbeat of the drone on it.
+        # This handler has a drone on it.
         drone_id = self._handlers_to_drones[handler_id]
-        idle_secs = time.time() - self._drones_heartbeat[drone_id]
-        if idle_secs > MAX_IDLE_SECS:
-          # The drone left, and this handler is avaiable again.
-          self._drones_to_handlers.pop(drone_id, None)
-          self._handlers_to_drones.pop(handler_id, None)
-          self._drones_heartbeat.pop(drone_id, None)
-          self._available_handlers.append(handler_id)
-          print ('no heartbeat from drone id=%s received for %0.1f seconds, '+
-                 'dropping and freeing drone_handler id=%s (%d ' +
-                 'available handlers)') % (
-                 drone_id, idle_secs, handler_id, len(self._available_handlers))
-          self._handlers_socket.send(b'FREE')
-        else:
-          self._handlers_socket.send(b'OK+%s' % drone_id)
+        self._handlers_socket.send(b'OK+%s' % drone_id)
       else:
-          # This is an existing available handler.
-          self._handlers_socket.send(b'FREE')
+        # This is an existing available handler.
+        self._handlers_socket.send(b'FREE')
       self._handlers_heartbeat[handler_id] = time.time()
-
-  def _remove_non_existing_handlers(self):
-    """Remove available handlers who are actually gone.
-    """
-    for handler_id in self._available_handlers:
-      idle_secs = time.time() - self._handlers_heartbeat[handler_id]
-      if idle_secs > MAX_IDLE_SECS:
-        self._available_handlers.remove(handler_id)
-        self._handlers_heartbeat.pop(handler_id, None)
-        print ('no heartbeat from drone_hander id=%s received for %0.1f ' +
-               'seconds, (%d available handlers)') %(
-              handler_id, idle_secs, len(self._available_handlers))
 
   def _process_drones_socket(self, all_sockets):
     """Process connections to self._drones_socket
     """
     if self._drones_socket in all_sockets:
       drone_id = self._drones_socket.recv()
-      if not drone_id in self._drones_to_handlers:
+      if drone_id in self._drones_to_drop:
+        # The drone's handler left.
+        self._drones_to_drop.remove(drone_id)
+        self._drones_socket.send(b'DROP')
+      elif not drone_id in self._drones_to_handlers:
         # This is a newly arrived drone.
         if self._available_handlers:
           # We have a handler for it.
@@ -123,22 +145,9 @@ class Manager(object):
           self._drones_socket.send(b'DROP')
       else:
         handler_id = self._drones_to_handlers[drone_id]
-        idle_secs = time.time() - self._handlers_heartbeat[handler_id]
-        if idle_secs > MAX_IDLE_SECS:
-          # The drone's handler has left.
-          self._drones_heartbeat.pop(drone_id, None)
-          self._handlers_heartbeat.pop(handler_id, None)
-          self._drones_to_handlers.pop(drone_id, None)
-          self._handlers_to_drones.pop(handler_id, None)
-          print ('no heartbeat from drone_hander id=%s received for %0.1f ' +
-                 'seconds, dropping and dropping drone id=%s (%d ' +
-                 'available handler)') % (
-                 handler_id, idle_secs, drone_id, len(self._available_handlers))
-          self._drones_socket.send(b'DROP')
-        else:
-          # The drone is in good status.
-          self._drones_heartbeat[drone_id] = time.time()
-          self._drones_socket.send(b'OK+%s' % handler_id)
+        # The drone is in good status.
+        self._drones_heartbeat[drone_id] = time.time()
+        self._drones_socket.send(b'OK+%s' % handler_id)
 
 
 def main(argv):
